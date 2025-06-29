b5d37688df831b1a239ddb65cae3d725683c0b81
Marcos Lima <mrsk1414@gmail.com>
2025-06-29 19:18:53 -0300

    feat: create sql database

---
+++ sql/script.sql
@@ -0,0 +1,30 @@
+CREATE TABLE carriers (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL UNIQUE,
+  code INT NOT NULL
+);
+
+INSERT INTO carriers (name, code) VALUES ('Vivo', 15);
+INSERT INTO carriers (name, code) VALUES ('Tim', 41);
+INSERT INTO carriers (name, code) VALUES ('Oi', 31);
+INSERT INTO carriers (name, code) VALUES ('Claro', 21);
+
+CREATE TABLE documents (
+  id SERIAL PRIMARY KEY,
+  cpf TEXT NOT NULL UNIQUE
+);
+
+CREATE TABLE phones (
+  id SERIAL PRIMARY KEY,
+  contact_id INTEGER NOT NULL REFERENCES documents(id),
+  phone TEXT NOT NULL UNIQUE,
+  carrier_id INTEGER NOT NULL REFERENCES carriers(id),
+  name TEXT NOT NULL,
+  description TEXT NOT NULL
+);
+
+CREATE TABLE recharges (
+  id SERIAL PRIMARY KEY,
+  phone_id INTEGER NOT NULL REFERENCES phones(id),
+  price INTEGER NOT NULL CHECK (price >= 1000 and price <= 100000)
+);
